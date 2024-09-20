ea19ff3bb4a43b5cc4d05733f803086e76bf97ba
Raj <patel.raj.7021@gmail.com>
2024-09-19 23:48:12 -0400

    Dockerize

--- sentiment_analysis/analysis_script.py
+++ sentiment_analysis/analysis_script.py
@@ -7,6 +7,8 @@ Created on Wed Sep 18 23:07:07 2024
 
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 import os
+import torch as pt
+import numpy as np
 
     
 def embed_sequences(input_seqs):
@@ -33,7 +35,7 @@ if __name__ == '__main__':
     
     
     
-    test_file = os.listdir('article_temp_files')[0]
+    test_file = os.listdir('article_temp_files')[1]
     with open(os.path.join('article_temp_files', test_file), 'r') as file:
         text = file.read().split('.')
         
@@ -44,12 +46,26 @@ if __name__ == '__main__':
     # bag of positive sentiment words to compare against
     pos_words = ['positive',
                  'optimistic',
-                 'hopeful'
-                 ]
+                 'hopeful']
     pos_sequences, pos_model, pos_embeddings = embed_sequences(pos_words)
     
-    # bag of negative sentiment words
+    # bag of negative sentiment words, must be same num as pos_words
     neg_words = ['negative',  
                  'pessimistic',
                  'cautious']
     neg_sequences, neg_model, neg_embeddings = embed_sequences(neg_words)
+    
+    
+    num_sentences = embeddings.shape[0]
+    num_comp_words = pos_embeddings.shape[0]
+    pos_comps = []
+    neg_comps = []
+    for i in range(num_sentences):       
+        for j in range(num_comp_words):
+            pos_comps.append(pt.dot(embeddings[i, 0, :], pos_embeddings[j, 0, :]).item())
+            neg_comps.append(pt.dot(embeddings[i, 0, :], neg_embeddings[j, 0, :]).item())
+    print(np.mean(pos_comps))
+    print(np.mean(neg_comps))
+    print(np.median(pos_comps))
+    print(np.median(neg_comps))
+    
