9ec791bd9042fe7eb184d694395160c2f63a0fec
Raj <patel.raj.7021@gmail.com>
2024-09-30 13:52:12 -0400

    Create summary record upon user input

--- sentiment_analysis/sentiment_app/views.py
+++ sentiment_analysis/sentiment_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Avg
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import AnalyzeRequestSerializer, SummariesSerializer, ArticlesSerializer
 from .models import Summaries, Articles
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 sys.path.append('../sentiment_analysis')
 from analysis_module import analysis_wrapper
@@ -37,7 +38,8 @@ class AnalyzeRequestView(APIView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             ticker = serializer.data.get('ticker')
-            current_date = datetime.today().strftime('%Y-%m-%d')
+            # current_date = datetime.today().strftime('%Y-%m-%d')
+            current_date = datetime.today()
             # run analysis
             article_records = analysis_wrapper(ticker)
             for article_record in article_records:
@@ -46,17 +48,25 @@ class AnalyzeRequestView(APIView):
                 query = Articles.objects.filter(link=article_record.link)
                 if not query.exists():                   
                     # only create new record if it doesn't
-                    new_record = Articles(ticker=ticker,
-                                        title=article_record.title,
-                                        date=article_record.date,
-                                        link=article_record.link,
-                                        pos_score=article_record.pos_score,
-                                        neg_score=article_record.neg_score,
-                                        overall_rating=article_record.overall_rating)
+                    new_record = Articles(ticker=ticker, 
+                                          title=article_record.title,
+                                          date=article_record.date,
+                                          link=article_record.link,
+                                          pos_score=article_record.pos_score,
+                                          neg_score=article_record.neg_score,
+                                          overall_rating=article_record.overall_rating)
                     new_record.save()
-                
-            
-            # new_summary = Summaries(ticker=ticker, date=current_date, overall_rating=overall_rating)
-            # new_summary.save()
+            past_week_articles = Articles.objects.filter(date__range=[current_date-timedelta(days=7),
+                                                                      current_date])
+            avg_rating = int(past_week_articles.aggregate(Avg("overall_rating"))['overall_rating__avg'])
+            query = Summaries.objects.filter(ticker=ticker, date=current_date.strftime('%Y-%m-%d'))
+            if query.exists():
+                # update record for the day if it exists
+                query.overall_rating = avg_rating
+                query.save(update_fields=['overall_rating'])
+            else:
+                # create a new one if it doesn't
+                new_summary = Summaries(ticker=ticker, date=current_date, overall_rating=avg_rating)
+                new_summary.save()
                 
             return Response(status=status.HTTP_201_CREATED)
